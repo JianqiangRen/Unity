dd07e9d757a763df9d7870fa8bc20641b3038180
任健强 <rjq235@163.com>
2017-03-21 16:57:57 +0800

    upload typical usage of Asset Bundle module

---
+++ AssetBundle   Usage/CreateAssetBundle/CreateAssetBundles.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllABs()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneOSXUniversal);
+    }
+
+    [MenuItem("Assets/Build Asset Bundles Using BuildMap")]
+    static void BuildMapABs()
+    {
+        // Create the array of bundle build details.
+        AssetBundleBuild[] buildMap = new AssetBundleBuild[1];
+
+        buildMap[0].assetBundleName = "enemybundle";
+
+        string[] enemyAssets = new string[2];
+        enemyAssets[0] = "Assets/Prefab/Cube.prefab";
+        enemyAssets[1] = "Assets/Prefab/Sphere.prefab";
+
+        buildMap[0].assetNames = enemyAssets;
+
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", buildMap, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+
+}
